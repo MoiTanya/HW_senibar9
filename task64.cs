1812b5f4b7a23d82a6e3933987587da33c8d2469
MoiTanya <130609745+MoiTanya@users.noreply.github.com>
2023-06-12 19:54:02 +0300

    Add files via upload
    task64 & task 66 commit

---
+++ task64.cs
@@ -0,0 +1,25 @@
+﻿// Задача 64: Задайте значение N. Напишите программу, которая выведет все натуральные числа в промежутке от N до 1. Выполнить с помощью рекурсии.
+
+// N = 5 -> "5, 4, 3, 2, 1"
+// N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1"
+
+void PrintNumbers(int N)
+{
+    if (N>1)
+    {
+        Console.Write($"{N}, ");
+        N--;
+        PrintNumbers(N);
+    }
+    else if(N==1)
+         Console.Write($"{N}.");
+    
+    else return;
+}
+
+Console.WriteLine("Программа выводит натуральные числа от N до 1, используя рекурсию функции");
+Console.WriteLine("Введите число N");
+int N = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine();
+Console.Write($"N = {N} -> ");
+PrintNumbers(N);
---
+++ task66.cs
@@ -0,0 +1,30 @@
+﻿// Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+
+void SummNumbers(int M, int N, int Summ)
+{
+    if (M<=N)
+    {
+        Summ = Summ + M;
+        M++;
+        SummNumbers(M, N, Summ);
+    }
+    
+    else 
+    {
+        Console.Write($"{Summ}");
+        return;
+    } 
+}
+
+Console.WriteLine("Программа вычисления суммы натуральных элементов в промежутке от M до N");
+Console.WriteLine("Введите число M");
+int M = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число N");
+int N = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine();
+int Summ = 0;
+Console.Write($"M = {M}; N = {N} -> ");
+SummNumbers(M, N, Summ);
